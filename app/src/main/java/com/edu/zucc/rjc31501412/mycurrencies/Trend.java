7f3fc99c4806f43c61fe5a7a531d6469b27d3e68
ChannRen <32730623+ChannRen@users.noreply.github.com>
2018-07-10 14:19:38 +0800

    add Trend

---
+++ app/src/main/java/com/edu/zucc/rjc31501412/mycurrencies/Trend.java
@@ -0,0 +1,13 @@
+package com.edu.zucc.rjc31501412.mycurrencies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trend);
+    }
+}
